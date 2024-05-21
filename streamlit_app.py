3e27ecde5785797302f7fe566ccaecc16ea2429c
allanmukhwana <allanmukhwana@gmail.com>
2024-05-21 22:55:48 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,7 +14,7 @@ with st.sidebar:
     st.title('Snowflake Arctic')
     if 'REPLICATE_API_TOKEN' in st.secrets:
         #st.success('API token loaded!', icon='✅')
-        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+        replicate_api = st.secrets['00cb329c95ba09385b0dcd8ace88ccc8451690ca']
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
